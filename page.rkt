dd88e7d9eee5a66b4ed4e9ad8f21b66fa6089782
Malcolm <malcolm.still@gmail.com>
2015-08-21 13:18:47 +0100

    Completed Page type

--- page.rkt
+++ page.rkt
@@ -3,20 +3,31 @@
 (require "object.rkt")
 
 (provide (all-defined-out))
-#|
 
+#|
 Each page of the PDF is represented by a page object. The page object
 is a dictionary.
+|#
 
+#|
+Is my type pedantry too much?
+
+Maybe the optional values taking up space will cause issues later on with memory.
+
+Leave it for the moment and we can always just go back to plain dictionaries for
+everything once it's working. For the moment it will probably help me get things
+correct. Correctness then worry about performance.
+
+If we could use macros to expand into type defitions we could generate all
+permutations of optional dictionary entries. Though that could potentially generate
+a lot of type defintions. TODO: work out how many permutations that would be.
 |#
 
 
-
-#|
-(define-type PageObject
+(define-type Page
   (List
    (Pairof 'Type 'Page)
-   (Pairof 'Parent 'IndirectReference)
+   (Pairof 'Parent (Indirect Dictionary))
    (Pairof 'LastModified (U String PDFNull))
    (Pairof 'Resources (U Dictionary PDFNull))
    (Pairof 'MediaBox (U (Arrayof Number) PDFNull))
@@ -25,6 +36,27 @@ is a dictionary.
    (Pairof 'TrimBox (U (Arrayof Number) PDFNull))
    (Pairof 'ArtBox (U (Arrayof Number) PDFNull))
    (Pairof 'BoxColorInfo (U Dictionary PDFNull))
-   (Pairof 'Contents (U Stream 
-|#
+   (Pairof 'Contents (U Stream (Arrayof Stream) PDFNull))
+   (Pairof 'Rotate (U Integer PDFNull))
+   (Pairof 'Group (U Dictionary PDFNull))
+   (Pairof 'Thumb (U Stream PDFNull))
+   (Pairof 'B (U Array PDFNull))
+   (Pairof 'Dur (U Number PDFNull))
+   (Pairof 'Trans (U Dictionary PDFNull))
+   (Pairof 'Annots (U (Arrayof (Indirect Dictionary)) PDFNull))
+   (Pairof 'AA (U Dictionary PDFNull))
+   (Pairof 'Metadata (U Stream PDFNull))
+   (Pairof 'PieceInfo (U Dictionary PDFNull))
+   (Pairof 'StructParents (U Integer PDFNull))
+   (Pairof 'ID (U String PDFNull)) ; spec says "byte string"
+   (Pairof 'PZ (U Number PDFNull))
+   (Pairof 'SeparationInfo (U Dictionary PDFNull))
+   (Pairof 'Tabs (U 'R 'C 'S PDFNull))
+   (Pairof 'TemplateInstantiated (U Symbol PDFNull))
+   (Pairof 'PresSteps (U Dictionary PDFNull))
+   (Pairof 'UserUnit (U Number PDFNull)) ; default to 1/72 inch (1 pt)
+   (Pairof 'VP (U Dictionary PDFNull))))
+   
+   
+   
 
